a6f1740ad81b447c262995552d1b11837b4bf439
Tomasz <tomasz9531@gmail.com>
2020-12-29 08:57:32 +0100

    Add user page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { useEffect } from "react";
 import './App.css';
+import {
+    Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import { useDispatch } from 'react-redux'
+
+import history from './history';
+
+import Navbar from './components/navigation/Navbar';
+
+import HomePage from './pages/HomePage';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
+import MainPage from './pages/MainPage';
+import UserPage from './pages/UserPage';
+
+import { SET_USER_DATA } from './redux/actions/User';
+
+// Restore user data
+let userData = localStorage.getItem('user');
+userData = JSON.parse(userData);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dispatch = useDispatch();
+    useEffect(() => {
+        console.log('useEffect: ', userData);
+        dispatch({ type: SET_USER_DATA, payload: { user: userData } });
+    })
+    return (
+        <Router history={history}>
+            <Navbar />
+            <Switch>
+                <Route exact path="/">
+                    {userData ? <MainPage /> : <HomePage />}
+                </Route>
+                <Route path="/register">
+                    <RegisterPage />
+                </Route>
+                <Route path="/login">
+                    <LoginPage />
+                </Route>
+                <Route path="/dashboard">
+                    <DashboardPage />
+                </Route>
+                <Route path="/users/">
+                    <UserPage />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
